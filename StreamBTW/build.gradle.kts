239e644b824634a23c578a2e9562fd78c8df20b1
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-03-09 18:03:26 +0000

    Update build.gradle.kts

--- StreamBTW/build.gradle.kts
+++ StreamBTW/build.gradle.kts
@@ -6,7 +6,7 @@ cloudstream {
     language = "en"
     // All of these properties are optional, you can safely remove them
 
-     description = "Live streams from the StreamBTW.
+     description = "Live streams from the StreamBTW."
     authors = listOf("Ben")
 
     /**
