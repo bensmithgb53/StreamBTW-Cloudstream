17666e35b0c5ac360cc7bdaff6176ee9764bba5c
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-03-11 10:24:47 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,10 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:8.6.0")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
@@ -26,7 +23,6 @@ allprojects {
 }
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
-
 fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
@@ -35,7 +31,6 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/bensmithgb53/StreamBTW-Cloudstream/")
     }
 
@@ -43,19 +38,16 @@ subprojects {
         namespace = "ben.smith53"
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
+            compileSdk = 35
             targetSdk = 35
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-
         tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "1.8" // Required
-                // Disables some unnecessary features
+                jvmTarget = "1.8"
                 freeCompilerArgs = freeCompilerArgs +
                         "-Xno-call-assertions" +
                         "-Xno-param-assertions" +
@@ -67,18 +59,25 @@ subprojects {
     dependencies {
         val apk by configurations
         val implementation by configurations
-
-        // Stubs for all Cloudstream classes
         apk("com.lagradost:cloudstream3:pre-release")
-
-        // these dependencies can include any of those which are added by the app,
-        // but you dont need to include any of them if you dont need them
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
-        implementation(kotlin("stdlib")) // adds standard kotlin features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
-        implementation("org.jsoup:jsoup:1.18.1") // html parser
+        implementation(kotlin("stdlib"))
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.1")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    }
+
+    // Add task to copy .cs3 to builds/
+    tasks.register("copyPluginToBuilds") {
+        dependsOn("buildPlugin") // Assumes Cloudstream plugin defines this task
+        doLast {
+            val buildsDir = file("$rootDir/builds")
+            buildsDir.mkdirs()
+            copy {
+                from("$buildDir/outputs/Strimsy.cs3") // Adjust path if needed
+                into(buildsDir)
+            }
+        }
     }
 }
 
