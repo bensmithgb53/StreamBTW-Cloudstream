115f5a2fb1b36e862b7b0a6c5ae04efbe10660cc
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-03-20 21:45:41 +0000

    Update PPVLandPlugin.kt

--- PPVLand/src/main/kotlin/ben/smith53/PPVLandPlugin.kt
+++ PPVLand/src/main/kotlin/ben/smith53/PPVLandPlugin.kt
@@ -3,6 +3,7 @@ package ben.smith53
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
+import ben.smith53.extractors.PPVLandExtractor // Add this import
 
 @CloudstreamPlugin
 class PPVLand : Plugin() {
