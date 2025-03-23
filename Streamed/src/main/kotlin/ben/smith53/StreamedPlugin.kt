6f7a03df4c7541b57836bec4de846a742c0dbea7
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-03-23 15:15:16 +0000

    Create StreamedPlugin.kt

---
+++ Streamed/src/main/kotlin/ben/smith53/StreamedPlugin.kt
@@ -0,0 +1,13 @@
+package ben.smith53
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class StreamedPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(StreamedProvider())
+        // Removed registerExtractorAPI(PPVLandExtractor())
+    }
+}
