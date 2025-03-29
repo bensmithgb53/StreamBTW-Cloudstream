5661c447ce348068ba05bc55c21cc2d2baff519c
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-03-29 10:59:30 +0000

    Update StreamedPlugin.kt

--- Streamed/src/main/kotlin/ben/smith53/StreamedPlugin.kt
+++ Streamed/src/main/kotlin/ben/smith53/StreamedPlugin.kt
@@ -2,11 +2,10 @@ package ben.smith53
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class StreamedPlugin : Plugin() {
-    override fun load(context: Context) {
-        registerMainAPI(StreamedProvider(context)) // Pass the context here
+class StreamedPlugin: Plugin() {
+    override fun load() {
+        registerMainAPI(StreamedProvider()) // No arguments
     }
 }
