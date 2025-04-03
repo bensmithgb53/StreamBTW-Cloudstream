2229321c35a9f31cbebddbe15a25334b2d4a4e2c
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-04-03 09:48:18 +0100

    Update StreamedPlugin.kt

--- Streamed/src/main/kotlin/ben/smith53/StreamedPlugin.kt
+++ Streamed/src/main/kotlin/ben/smith53/StreamedPlugin.kt
@@ -1,6 +1,7 @@
 package ben.smith53
 
-import com.lagradost.cloudstream3.registerMainAPI
+import com.lagradost.cloudstream3.MainAPI // Ensure this is imported
+import com.lagradost.cloudstream3.registerMainAPI // Explicit import
 
 fun main() {
     registerMainAPI(StreamedProvider())
