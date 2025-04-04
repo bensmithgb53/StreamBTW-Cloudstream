0c1b97ec865780b0f3e0e4773f2dca1acc0219ab
Owen <104525191+bensmithgb53@users.noreply.github.com>
2025-04-04 20:02:34 +0100

    Update PPVLandExtractor.kt

--- PPVLand/src/main/kotlin/ben/smith53/PPVLandExtractor.kt
+++ PPVLand/src/main/kotlin/ben/smith53/PPVLandExtractor.kt
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.module.kotlin.readValue
 
 class PPVLandExtractor : ExtractorApi() {
     override val name = "PPVLandExtractor"
-    override val mainUrl = "https://ppv.land"
+    override val mainUrl = "https://ppv.wtf"
     override val requiresReferer = true
 
     private val USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0"
